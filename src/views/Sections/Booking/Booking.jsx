573b40692bd212b5b5bab0f2c477267e23957c95
Daniel Rodriguez <drodriguez11375@gmail.com>
2021-10-01 12:09:45 -0400

    Added Formik form

--- src/views/Sections/Booking/Booking.jsx
+++ src/views/Sections/Booking/Booking.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
-
 import { Row, Col } from "react-bootstrap";
 import Icon from "components/Icon";
 import PageSection from "components/PageSection";
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 const Booking = ({ className, frontmatter }) => {
     if (!frontmatter) {
@@ -15,12 +15,40 @@ const Booking = ({ className, frontmatter }) => {
     return (
         <PageSection className={className} id={anchor}>
             <Row className="justify-content-center">
-                <form>
-                    <label htmlFor="testy">
-                        {header}<input id="testy" type="text"/>
-                    </label>
-                    <button type="submit">Go</button>
-                </form>
+                <div>
+                    <Formik
+                        initialValues={{ email: '', password: '' }}
+                        validate={values => {
+                        const errors = {};
+                        if (!values.email) {
+                            errors.email = 'Required';
+                        } else if (
+                            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                        ) {
+                            errors.email = 'Invalid email address';
+                        }
+                        return errors;
+                        }}
+                        onSubmit={(values, { setSubmitting }) => {
+                        setTimeout(() => {
+                            alert(JSON.stringify(values, null, 2));
+                            setSubmitting(false);
+                        }, 400);
+                        }}
+                    >
+                        {({ isSubmitting }) => (
+                        <Form>
+                            <Field type="email" name="email" />
+                            <ErrorMessage name="email" component="div" />
+                            <Field type="password" name="password" />
+                            <ErrorMessage name="password" component="div" />
+                            <button type="submit" disabled={isSubmitting}>
+                            Submit
+                            </button>
+                        </Form>
+                        )}
+                    </Formik>
+                </div>
             </Row>
         </PageSection>
     );
