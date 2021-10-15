4442c935db857ae655599f20fbd5b346ad4c2291
Daniel Rodriguez <drodriguez11375@gmail.com>
2021-10-15 14:57:22 -0400

    Removed commented-out exports

--- src/views/Sections/index.js
+++ src/views/Sections/index.js
@@ -1,8 +1,8 @@
-// export { default as About } from "./About";
+export { default as About } from "./About";
 export { default as Contact } from "./Contact";
 export { default as Gallery } from "./Gallery";
 export { default as Services } from "./Services";
 export { default as Team } from "./Team";
 export { default as Booking } from "./Booking";
-// export { default as Clients } from "./Clients";
+export { default as Clients } from "./Clients";
 export { default as Shop } from "./Shop";
