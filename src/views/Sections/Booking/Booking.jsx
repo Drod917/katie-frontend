7a106a516da89a8b61e0b1a0bccb34a679fd7dff
Daniel Rodriguez <drodriguez11375@gmail.com>
2021-10-15 14:59:16 -0400

    Reordered Booking to use BookingForm component

--- src/views/Sections/Booking/Booking.jsx
+++ src/views/Sections/Booking/Booking.jsx
@@ -1,88 +1,43 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { FormControl, FormGroup, FormLabel, Row, Col } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
 import PageSection from "components/PageSection";
-import SectionHeader from "components/SectionHeader";
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
+import BookingForm from "components/BookingForm";
+import "./Booking.scss"
+import Icon from "components/Icon";
 
 const Booking = ({ className, frontmatter }) => {
     if (!frontmatter) {
         return null;
     }
     const phoneRegExp = /(^([1-9]{3}[-]?)([1-9]{3}[-]?)([1-9]{4})$)|(^\(([1-9]{3}\)[-]?)([1-9]{3}[-]?)([1-9]{4})$)/
-    const { anchor, header, subheader, telephone, email } = frontmatter;
+    const { anchor, header, subheader, telephone } = frontmatter;
 
     return (
         <PageSection className={className} id={anchor}>
-            <Row>
-                <SectionHeader header="Book Now" subheader="Enter Booking Info Here" />
-            </Row>
             <Row className="justify-content-center">
-                <Formik
-                    initialValues={{ fullName: '', phoneNumber: '', email: '' }}
-                    validationSchema={Yup.object({
-                        fullName: Yup.string()
-                            .max(30, 'Must be 30 characters or less')
-                            .required('Required'),
-                        phoneNumber: Yup.string()
-                            .matches(phoneRegExp, 'Format: 012-345-6789')
-                            .required('Required'),
-                        email: Yup.string().email(' ').required('Required')
-                    })}
-                    onSubmit={(values, { setSubmitting }) => {
-                        setTimeout(() => {
-                            alert(JSON.stringify(values, null, 2));
-                            setSubmitting(false);
-                        }, 400);
-                    }}
-                >
-                    {({
-                        handleSubmit,
-                        handleChange,
-                        handleBlur,
-                        values,
-                        touched,
-                        isValid,
-                        errors,
-                    }) => (
-                        <Form>
-                            <FormGroup controlId="fullName">
-                                <FormLabel>Full Name</FormLabel>
-                                <FormControl 
-                                    type="text"
-                                    value={values.fullName}
-                                    onChange={handleChange}
-                                    isValid={touched.fullName && !errors.fullName}/>
-                                {/* ErrorMessage name references validationSchema */}
-                                <ErrorMessage name="fullName"/>
-                            </FormGroup>
-                            <FormGroup controlId="phoneNumber">
-                                <FormLabel>Phone Number</FormLabel>
-                                <FormControl 
-                                    type="text"
-                                    value={values.phoneNumber}
-                                    onChange={handleChange}
-                                    isValid={touched.fullName && !errors.phoneNumber}/>
-                                {/* ErrorMessage name references validationSchema */}
-                                <ErrorMessage name="phoneNumber"/>
-                            </FormGroup>
-                            <FormGroup controlId="email">
-                                <FormLabel>Email Address</FormLabel>
-                                <FormControl 
-                                    type="text"
-                                    value={values.email}
-                                    onChange={handleChange}
-                                    isValid={touched.fullName && !errors.email}/>
-                                {/* ErrorMessage name references validationSchema */}
-                                <ErrorMessage name="email"/>
-                            </FormGroup>
-                            <Row className="justify-content-center">
-                                <button className="styled-btn" type="submit">Submit</button>
-                            </Row>
-                        </Form>
-                    )}
-                </Formik>
+                <Col lg={8} className="text-center">
+                    <h2 className="mt-0">{header}</h2>
+                    <hr className="divider my-4" />
+                    <p className="text-muted mb-5">{subheader}</p>
+                </Col>
+            </Row>
+            <Row className="justify-content-around">
+                <Col xs={10} md={3}>
+                    <BookingForm/>
+                </Col>
+                <Col xs={10} md={3} className="contact-info">
+                    <Row className="justify-content-end">
+                        <Col xs={10} className="d-flex justify-content-end">
+                            <h5>{telephone}</h5>
+                        </Col>
+                    </Row>
+                    <Row className="justify-content-end">
+                        <Col className="d-flex justify-content-end">
+                            salonEmail@salonEmail.com
+                        </Col>
+                    </Row>
+                </Col>
             </Row>
         </PageSection>
     );
