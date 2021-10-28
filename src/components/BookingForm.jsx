652699b80ae6d59a47c786760b916499d1f38b3e
Daniel Rodriguez <drodriguez11375@gmail.com>
2021-10-28 00:00:40 -0400

    Added asynchronous calls to enable form submission

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -3,25 +3,94 @@ import { FormControl, FormGroup, FormLabel, Row, Col } from "react-bootstrap";
 import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
 
+
+const isResponseOk = (res) => {
+    return !(res.status !== 200 && res.status !== 201);
+  }
+
+const createBooking = async (booking) => {
+    const requestBody = {
+        query: `
+            mutation CreateBooking(
+                $fullname: String!,
+                $phone: String!,
+                $email: String!
+                $date: String!,
+                $service: String!,
+                $comment: String!,
+            )   {
+                createBooking(bookingInput: {
+                    fullname: $fullname,
+                    phone: $phone,
+                    email: $email,
+                    date: $date,
+                    service: $service,
+                    comment: $comment
+                    }) {
+                        _id
+                        fullname
+                        phone
+                        email
+                        date
+                        service
+                        comment
+                        confirmed
+                    }
+                }
+        `,
+        variables: {
+            ...booking
+        }
+    };
+
+    try {
+        const response = await fetch('http://localhost:8002/graphql', {
+            method: 'POST',
+            body: JSON.stringify(requestBody),
+            headers: {
+            'Content-Type': 'application/json',
+            }
+        });
+
+        if (!isResponseOk(response)) {
+            throw new Error('Create Event Failed!');
+        }
+
+        const { data } = await response.json();
+        // There may be an edge case where there's a failing client token
+        if (!data.createBooking) {
+            console.log("Authentication Error");
+            return false;
+        }
+        return { ...data.createBooking };
+    } catch (e) {
+    console.log(e);
+    return false;
+    }
+}
+
+
+
 const BookingForm = () => {
 
     const phoneRegExp = /(^([1-9]{3}[-]?)([1-9]{3}[-]?)([1-9]{4})$)|(^\(([1-9]{3}\)[-]?)([1-9]{3}[-]?)([1-9]{4})$)/
 
     return (
         <Formik
-            initialValues={{ fullName: '', phoneNumber: '', email: '' }}
+            initialValues={{ fullname: '', phone: '', email: '', date: '', service: '', comment: '' }}
             validationSchema={Yup.object({
-                fullName: Yup.string()
+                fullname: Yup.string()
                     .max(30, 'Must be 30 characters or less')
                     .required('Required'),
-                phoneNumber: Yup.string()
+                phone: Yup.string()
                     .matches(phoneRegExp, 'Ex. 012-345-6789')
                     .required('Required'),
                 email: Yup.string().email(' ').required('Required')
             })}
             onSubmit={(values, { setSubmitting }) => {
                 setTimeout(() => {
-                    alert(JSON.stringify(values, null, 2));
+                    // alert(JSON.stringify(values, null, 2));
+                    createBooking(values);
                     setSubmitting(false);
                 }, 400);
             }}
@@ -36,30 +105,30 @@ const BookingForm = () => {
                 errors,
             }) => (
                 <Form>
-                    <FormGroup controlId="fullName">
+                    <FormGroup controlId="fullname">
                         <FormLabel>Full Name</FormLabel>
                         <FormControl 
                             size="md"
                             type="text"
-                            value={values.fullName}
+                            value={values.fullname}
                             onChange={handleChange}
-                            isValid={touched.fullName && !errors.fullName}
-                            isInvalid={touched.fullName && errors.fullName}/>
+                            isValid={touched.fullname && !errors.fullname}
+                            isInvalid={touched.fullname && errors.fullname}/>
                         {/* ErrorMessage name references validationSchema */}
-                        {/* <ErrorMessage name="fullName"/> */}
+                        {/* <ErrorMessage name="fullname"/> */}
                         <FormControl.Feedback type="invalid">
                             Required
                         </FormControl.Feedback>
                     </FormGroup>
-                    <FormGroup controlId="phoneNumber">
+                    <FormGroup controlId="phone">
                         <FormLabel>Phone Number</FormLabel>
                         <FormControl 
                             size="md"
                             type="text"
-                            value={values.phoneNumber}
+                            value={values.phone}
                             onChange={handleChange}
-                            isValid={touched.phoneNumber && !errors.phoneNumber}
-                            isInvalid={touched.phoneNumber && errors.phoneNumber}/>
+                            isValid={touched.phone && !errors.phone}
+                            isInvalid={touched.phone && errors.phone}/>
                         {/* ErrorMessage name references validationSchema */}
                         <FormControl.Feedback type="invalid">
                             Required
@@ -78,6 +147,45 @@ const BookingForm = () => {
                         {/* <ErrorMessage name="email"/> */}
                         <FormControl.Feedback type="invalid">
                             {values.email === "" ? "Required" : "Invalid Email Address"}
+                        </FormControl.Feedback>
+                    </FormGroup>
+                    <FormGroup controlId="date">
+                        <FormLabel>Date</FormLabel>
+                        <FormControl 
+                            size="md"
+                            type="date"
+                            value={values.date}
+                            onChange={handleChange}
+                            isValid={touched.date && !errors.date}
+                            isInvalid={touched.date && errors.date}/>
+                        <FormControl.Feedback type="invalid">
+                            Required
+                        </FormControl.Feedback>
+                    </FormGroup>
+                    <FormGroup controlId="service">
+                        <FormLabel>Service</FormLabel>
+                        <FormControl 
+                            size="md"
+                            type="text"
+                            value={values.service}
+                            onChange={handleChange}
+                            isValid={touched.service && !errors.service}
+                            isInvalid={touched.service && errors.service}/>
+                        <FormControl.Feedback type="invalid">
+                            Required
+                        </FormControl.Feedback>
+                    </FormGroup>
+                    <FormGroup controlId="comment">
+                        <FormLabel>Comment</FormLabel>
+                        <FormControl 
+                            size="md"
+                            type="text"
+                            value={values.comment}
+                            onChange={handleChange}
+                            isValid={touched.comment && !errors.comment}
+                            isInvalid={touched.comment && errors.comment}/>
+                        <FormControl.Feedback type="invalid">
+                            Required
                         </FormControl.Feedback>
                     </FormGroup>
                     <Row className="justify-content-center" style={{ paddingTop:"2.5rem" }}>
