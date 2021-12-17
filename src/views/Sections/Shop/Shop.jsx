9332b4bbe995789cc6d4056cec1f5caa4aab3fb9
Daniel Rodriguez <drodriguez11375@gmail.com>
2021-12-17 04:35:07 -0500

    Added styling

--- src/views/Sections/Shop/Shop.jsx
+++ src/views/Sections/Shop/Shop.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import clsx from "clsx";
 
 import { Button, Row, Col } from "react-bootstrap";
 import Icon from "components/Icon";
@@ -15,7 +16,7 @@ const Shop = ({ className, frontmatter }) => {
     const { anchor, header, subheader, telephone, email } = frontmatter;
 
     return (
-        <PageSection id={anchor}>
+        <PageSection className={className} id={anchor}>
             <SectionHeader header={header} subheader={subheader}/>
             <Row className="justify-content-center shop-btn">
                 <Button size="xl" variant="primary" className="text-uppercase" href="https://shop.saloninteractive.com/store/KBHAIR/product_categories?group=1">
